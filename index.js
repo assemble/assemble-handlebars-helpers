a7f4834556d079680cb9d00beec559138d1b00d0
Brian Woodward <brian.woodward@gmail.com>
2016-04-22 17:51:26 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-handlebars-helpers <https://github.com/assemble/assemble-handlebars-helpers>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function() {
+};
+
