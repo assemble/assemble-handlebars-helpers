0f1478a67fce0f6d3d1b08e1e767ed540bd72817
Brian Woodward <brian.woodward@gmail.com>
2016-04-22 18:24:26 -0400

    tests

---
+++ test.js
@@ -0,0 +1,107 @@
+'use strict';
+
+var capture = require('capture-stream');
+var assemble = require('assemble');
+var assert = require('assert');
+var helpers = require('./');
+var app;
+
+describe('assemble-handlebars-helpers', function() {
+  beforeEach(function() {
+    app = assemble();
+    app.helpers(helpers);
+  });
+
+  it('should register helpers', function() {
+    assert.deepEqual(Object.keys(app._.helpers.sync), ['each', 'if', 'log', 'lookup', 'unless', 'with']);
+  });
+
+  describe('each', function() {
+    it('should render using {{each}} helper', function(cb) {
+      app.page('a.hbs', { content: '{{#each items}}{{this}}{{/each}}' });
+      var context = {
+        items: ['a', 'b', 'c']
+      };
+      app.render('a.hbs', context, function(err, results) {
+        if (err) return cb(err);
+        assert.equal(results.content, 'abc');
+        cb();
+      });
+    });
+  });
+
+  describe('if', function() {
+    it('should render using {{if}} helper', function(cb) {
+      app.page('a.hbs', { content: '{{#if foo}}{{foo}}{{/if}}' });
+      var context = {
+        foo: 'bar'
+      };
+      app.render('a.hbs', context, function(err, results) {
+        if (err) return cb(err);
+        assert.equal(results.content, 'bar');
+        cb();
+      });
+    });
+  });
+
+  describe('log', function() {
+    it('should render using {{log}} helper', function(cb) {
+      app.page('a.hbs', { content: '{{log items}}' });
+      var context = {
+        items: ['a', 'b', 'c']
+      };
+
+      var restore = capture(process.stdout);
+      app.render('a.hbs', context, function(err, results) {
+        var output = restore(true);
+        if (err) return cb(err);
+        assert.equal(results.content, '');
+        assert.equal(output.indexOf("[ 'a', 'b', 'c' ]"), 0);
+        cb();
+      });
+    });
+  });
+
+  describe('lookup', function() {
+    it('should render using {{lookup}} helper', function(cb) {
+      app.page('a.hbs', { content: '{{#each (lookup data "items")}}{{this}}{{/each}}' });
+      var context = {
+        data: { items: ['a', 'b', 'c'] }
+      };
+      app.render('a.hbs', context, function(err, results) {
+        if (err) return cb(err);
+        assert.equal(results.content, 'abc');
+        cb();
+      });
+    });
+  });
+
+  describe('unless', function() {
+    it('should render using {{unless}} helper', function(cb) {
+      app.page('a.hbs', { content: '{{#unless foo}}{{bar}}{{/unless}}' });
+      var context = {
+        foo: false,
+        bar: 'baz'
+      };
+      app.render('a.hbs', context, function(err, results) {
+        if (err) return cb(err);
+        assert.equal(results.content, 'baz');
+        cb();
+      });
+    });
+  });
+
+  describe('with', function() {
+    it('should render using {{with}} helper', function(cb) {
+      app.page('a.hbs', { content: '{{#with data}}{{#each items}}{{this}}{{/each}}{{/with}}' });
+      var context = {
+        data: { items: ['a', 'b', 'c'] }
+      };
+      app.render('a.hbs', context, function(err, results) {
+        if (err) return cb(err);
+        assert.equal(results.content, 'abc');
+        cb();
+      });
+    });
+  });
+});
